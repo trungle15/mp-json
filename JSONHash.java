7cc33bfa308f7e3d94a9e5ed6a45d930666beef7
trungle15 <trungle15@users.noreply.github.com>
2024-05-03 00:06:56 -0500

    Add baseline code for Hash Table

--- JSONHash.java
+++ JSONHash.java
@@ -1,5 +1,6 @@
 import java.io.PrintWriter;
 import java.util.Iterator;
+import java.util.LinkedList;
 
 import javax.naming.InitialContext;
 
@@ -11,21 +12,24 @@ public class JSONHash implements JSONValue {
   // +--------+------------------------------------------------------
   // | Fields |
   // +--------+
-  KVPair<JSONString, JSONValue>[] table;
-  int capacity;
-  int size;
-
-  static final int INITIAL_CAPACITY = 16;
+  private LinkedList<KVPair<JSONString, JSONValue>>[] table;
+  private int size;
+  private static final int INITIAL_CAPACITY = 16;
+  private static final double LOAD_FACTOR = 0.75;
 
   // +--------------+------------------------------------------------
   // | Constructors |
   // +--------------+
   @SuppressWarnings("unchecked")
   public JSONHash() {
-    this.capacity = INITIAL_CAPACITY;
+    
+    table = (LinkedList<KVPair<JSONString, JSONValue>>[]) new LinkedList[INITIAL_CAPACITY];
+    for (int i = 0; i < INITIAL_CAPACITY; i++) {
+      table[i] = new LinkedList<>();
+    }
+
     this.size = 0;
-    this.table = new KVPair[capacity];
-  }
+  } 
 
   // +-------------------------+-------------------------------------
   // | Standard object methods |
@@ -74,11 +78,21 @@ public class JSONHash implements JSONValue {
   // | Hashtable methods |
   // +-------------------+
 
+  private int hash(JSONString key) {
+    int hashCode = key.hashCode();
+    return Math.abs(hashCode % table.length);
+  }
   /**
    * Get the value associated with a key.
    */
   public JSONValue get(JSONString key) {
-    return null;        // STUB
+    int index = hash(key);
+    for (KVPair<JSONString, JSONValue> pair : table[index]) {
+      if (pair.key().equals(key)) {
+        return pair.value();
+      }
+    }
+    return null;
   } // get(JSONString)
 
   /**
@@ -92,14 +106,28 @@ public class JSONHash implements JSONValue {
    * Set the value associated with a key.
    */
   public void set(JSONString key, JSONValue value) {
-                        // STUB
+    int keyhash = hash(key);
+    
+    // if key already exists in the table, update the value and return.
+    for (KVPair<JSONString, JSONValue> pair : table[keyhash]) {
+      if (pair.key().equals(key)) {
+        pair.setValue(value);
+        return;
+      } // if
+    } // for
+
+    // else if the value does not exist in the table, add the value to the correct index of 'table'
+    table[keyhash].add(new KVPair<JSONString,JSONValue>(key, value));
+    // then, increase the size
+    this.size++;
+
   } // set(JSONString, JSONValue)
 
   /**
    * Find out how many key/value pairs are in the hash table.
    */
   public int size() {
-    return 0;           // STUB
+    return this.size;           // STUB
   } // size()
 
 } // class JSONHash
